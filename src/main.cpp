b2be87dfae288215635ee0b5f8dfa1aeadcd3ba1
leraerra <valeriaberbasova@yandex.ru>
2026-05-31 18:48:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
+#include <string>
 #include "bst.h"
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
     return 0;
+  const char* file = "src/war_peace.txt";
+  BST<std::string> tree;
+  makeTree(tree, file);
+  printFreq(tree);
+  return 0;
 }
